9a181f67ca62765c0f6711b5163f54583ce7bd72
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-05-04 16:05:58 +0000

    v3

--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--Seletor de Caracteres--
-letra = input("Digite a Letra")
-if letra == "A":
-  print("Voce escolheu a letra A")
+print("Sistema Seletor de Caracteres 🔠")
+print("Escolha um letra e veja se esta disponivel 👀")
+letra = input("Digite a Letra: ").upper()
+if letra == "A":        
+  print("Voce escolheu a letra A 👌")
 elif letra == "B":
-  print("Voce escolheu a Letra B")
+  print("Voce escolheu a Letra B 👌")
 else:
-  print("Essa opcao nao esta disponivel")
+  print("Essa opcao nao esta disponivel ❌")
--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,6 @@
-
+#Exemplo de uso da variavel sentinela 
+while True: 
+ comando = input("Digite um Comando - Para parar digite 'sair'")
+ if comando == "sair":
+   break 
+print(f"Executando:{comando}")
