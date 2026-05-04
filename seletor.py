de39b6d51a4e3231ffa870e0ce7df65a44597141
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-05-04 09:33:13 -0300

    V1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de Caracteres--
+letra = input("Digite a Letra")
+if letra == "A":
+  print("Voce escolheu a letra A")
+elif letra == "B":
+  print("Voce escolheu a Letra B")
+else:
+  print("Essa opcao nao esta disponivel")
