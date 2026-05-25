a2d80544f2d30530d6d2fe5e1cd7a3771e906c56
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-05-25 12:27:47 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamando a Funcao
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTAO ESCOLAR')
+    print('Versao 1.0 - Desenvolvido no Codespaces')   
+    print('=' * 30)
+    
+### Chamando a funcao 
+exibir_cabecalho()
+print('Menu Principal')
