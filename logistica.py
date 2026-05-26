63897ff988cc560704e251dd84265060aeb7572c
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-05-26 15:54:39 +0000

    v1

--- logistica.py
+++ logistica.py
@@ -1,6 +1,14 @@
 #Calculadora de frete --
 def cabecalho ():
-    print("\n" + "=" * 30)
+    print("\n" + "=" *30)
     print("SISTEMA DE LOGISTICA")
-def calcular_frete(peso):
-    if peso >= 20 and peso > 20:
+def calcular_frete(peso_carga):
+    if peso_carga <= 20 :
+        return peso_carga * 10.00
+    else:
+        return peso_carga * 15.00
+cabecalho()
+peso_carga = float(input("Digite o Peso da Carga em (kg):"))
+frete = calcular_frete(peso_carga)
+print(F"O valor do frete é: R$ {frete:.2f}")
+print("=" * 30)
