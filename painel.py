660f887b7d28a379b94aaefd294cf2649a5da328
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-06-22 12:28:40 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,7 @@
+#O Painel Do Hospital
+print("--- PAINEL DE ATENDIMENTO ---")
+fila_espera = ["Senha 01", "Senha 02", "Senha 03", "Senha 04"]
+print(f"O proximo paciente a ser atendido é:{fila_espera[0]}")
+print(f"O segundo paciente a ser atendido é:{fila_espera[1]}")
+total = len(fila_espera)
+print(f"Total aguardando: {total}")
