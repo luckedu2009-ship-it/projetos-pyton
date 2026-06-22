0a9371cbfcaf8c9d868e3d4b80f374f86ca8465a
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-06-22 13:59:58 +0000

    v1

---
+++ podio.py
@@ -0,0 +1,7 @@
+# Lista dos vencedores
+vencedores = ["Davi", "Vicenzzo", "Guilherme"]
+
+# Exibindo o pódio
+print("Ouro:", vencedores [0])
+print("Prata:", vencedores [1])
+print("Bronze:", vencedores [2])
