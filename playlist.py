37ecf1bd0cd7b31c1e215371e853715695d68883
luckedu2009-ship-it <luckedu2009@gmail.com>
2026-06-22 12:40:17 +0000

    v1

---
+++ playlist.py
@@ -0,0 +1,7 @@
+print("Music Player")
+playlist = ["Musica 1 - Pagode", "Musica 2 - RAP", "Musica 3 - Sertanejo"]
+atual = 0
+print(f"Tocando Agora:{playlist[atual]}")
+print("Musica Anterior: -- (Inicio da Playlist)")
+print("=" * 40)
+            
